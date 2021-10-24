75057bf239c8b11bb8c38e92621b585b1cf3f9b7
baris <baris@kweak.ly>
2021-10-24 20:02:16 +0200

    Update azure-cli.sh

--- azure-cli.sh
+++ azure-cli.sh
@@ -34,8 +34,11 @@ az apservice plan create --name -g groupName -n appserviceplanName
 # create a new webapp
 az wepapp create -g groupName -n webappName -p webappplanName
 
-# open firewall
+# create a new firewall rule
 az sql server firewall-rule create --resource-group myResourceGroup --server <server-name> --name AllowAzureIps --start-ip-address 0.0.0.0 --end-ip-address 0.0.0.0
+
+# allow access from local computer
+az sql server firewall-rule create --name AllowLocalClient --server <server-name> --resource-group myResourceGroup --start-ip-address=<your-ip-address> --end-ip-address=<your-ip-address>
 
 # spin up a new webapp (within an exisiting html project)
 az webapp up --location eastus -name webappName --html
