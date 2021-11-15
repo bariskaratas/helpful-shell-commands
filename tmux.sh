0134f05c61beed0d957c79d14dfa32225cc2cfa9
baris <baris.karatas@gard.no>
2021-11-15 14:27:04 +0100

    Create tmux.sh

---
+++ tmux.sh
@@ -0,0 +1,28 @@
+# Below are some most common commands for managing Tmux windows and panes:
+
+# Create a new window (with shell)
+Ctrl+b c 
+
+# Choose window from a list
+Ctrl+b w 
+
+# Switch to window 0 (by number )
+Ctrl+b 0 
+
+# Rename the current window
+Ctrl+b , 
+
+# Split current pane horizontally into two panes
+Ctrl+b % 
+
+# Split current pane vertically into two panes
+Ctrl+b " 
+
+# Go to the next pane
+Ctrl+b o 
+
+# Toggle between the current and previous pane
+Ctrl+b ; 
+
+# Close the current pane
+Ctrl+b x 
