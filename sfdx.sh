c913837c6c243d6d0e1f16ef8c78a47cb56bbfc6
baris <baris.karatas@gard.no>
2022-05-25 13:48:13 +0200

    Update sfdx.sh

--- sfdx.sh
+++ sfdx.sh
@@ -1,2 +1,8 @@
 # list commands
 sfdx commands
+
+# retrieve custom objects | add -u for username when needed
+sfdx force:source:retrieve -m CustomObject 
+
+# create custom objects
+sfdx force:cmdt:create -n Mycmdt -d force-app/main/dirObjects
